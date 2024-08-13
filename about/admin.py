fc5faacb9f51b2848ac4510c6b018c192aae3cca
Erfan Najimi <erfannajimi123@gmail.com>
2024-08-13 08:22:52 +0000

    Register about model in admin.py with use of summernote for rich text editor

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+
+    list_display = ('title',)
+    summernote_fields = ('content',)
+
 # Register your models here.
